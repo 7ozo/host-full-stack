938cd2a5a2df50f7227d404206f0f0b538a17566
Velpar <hforhoozoo@gmail.com>
2022-05-13 21:03:03 +0200

    changing frontend env

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "http://udagrambackend-env.eba-strg3g6m.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
